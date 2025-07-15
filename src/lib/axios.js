f6788f233a15f0740c301c935930d4b79a0242b1
omaromarsalhi <omar.salhi.job@gmail.com>
2025-07-15 17:30:54 +0100

    updating the backedn url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+const backend_url = import.meta.env.BACKEND_URL || "http://localhost:3500";
 export const axiosInstance = axios.create({
-  baseURL: "http://SahraTalkFE:3500/api/v1",
+  baseURL: `${backend_url}/api/v1`,
   withCredentials: true,
 });
