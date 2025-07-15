bca23d35262a2e071d80736057bca14fe7ae966b
omaromarsalhi <omar.salhi.job@gmail.com>
2025-07-15 17:47:29 +0100

    fixing errors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const backend_url = import.meta.env.BACKEND_URL || "http://localhost:3500";
+// const backend_url = import.meta.env.BACKEND_URL || "http://localhost:3500";
+const backend_url = "https://omar.italynorth.cloudapp.azure.com";
 export const axiosInstance = axios.create({
   baseURL: `${backend_url}/api/v1`,
   withCredentials: true,
